e0743083a4300641372e04f0cace99650ee565c6
Francesco Paris <francesco.paris8@gmail.com>
2014-10-05 17:51:36 +0200

    primo
    primo

---
+++ spring-iniezione-dipendenze/README.md
@@ -0,0 +1,4 @@
+Iniezione Dipende Spring
+============================
+
+Progetto in cui si mostra l'iniezione delle dipendenze in Sprig
---
+++ spring-iniezione-dipendenze/mysqrtspring/src/main/java/it/uniroma3/dia/asw/service/SqrtService.java
@@ -0,0 +1,5 @@
+package it.uniroma3.dia.asw.service;
+
+public interface SqrtService {
+	double getSqrt(Double n) throws SqrtServiceException;
+}
